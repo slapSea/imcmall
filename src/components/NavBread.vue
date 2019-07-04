10783a446a762b7487936e011e2825f99cce00ec
linhaicheng <lhcjinhg44@163.com>
2019-07-04 22:46:36 +0800

    init project

---
+++ src/components/NavBread.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <slot name="bread"></slot>
+        <slot name="B"></slot>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBread"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.config.productionTip = false
+
+Vue.use('VueLazyLoad',function () {
+  loading:'@/static/loading-svg/loading-bars.svg'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
