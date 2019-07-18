924e6f1e5f357a9572f03deeee40b15285a6c671
linhaicheng <lhcjinhg44@163.com>
2019-07-18 23:21:18 +0800

    排序，加载更多

--- src/views/GoodLists.vue
+++ src/views/GoodLists.vue
@@ -10,7 +10,7 @@
           <div class="filter-nav">
             <span class="sortby">Sort by:</span>
             <a href="javascript:void(0)" class="default cur">Default</a>
-            <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+            <a href="javascript:void(0)" class="price" @click="sortGoods" >Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
             <a href="javascript:void(0)" class="filterby stopPop" @click="showFileterPop">Filter by</a>
           </div>
           <div class="accessory-result">
@@ -31,17 +31,20 @@
                 <ul>
                   <li v-for="(item,index) in goodList">
                     <div class="pic">
-                      <a href="#"><img v-lazy="'./static/'+item.productImg" alt=""></a>
+                      <a href="#"><img v-lazy="'./static/'+item.productImage" alt=""></a>
                     </div>
                     <div class="main">
                       <div class="name">{{item.productName}}</div>
-                      <div class="price">{{item.productPrice}}</div>
+                      <div class="price">{{item.salePrice}}</div>
                       <div class="btn-area">
                         <a href="javascript:;" class="btn btn--m">加入购物车</a>
                       </div>
                     </div>
                   </li>
                 </ul>
+                <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+                  加载中...
+                </div>
               </div>
             </div>
           </div>
@@ -77,7 +80,12 @@
             }],
             priceChecked:'all',
             filterBy:false,
-            overLayFlag:false
+            overLayFlag:false,
+            sortFlag:true,
+            page:1,
+            pageSize:8,
+            //默认是禁用
+            busy:true
           }
         },
         components:{
@@ -89,13 +97,28 @@
           this.getGoodLists()
         },
         methods:{
-          getGoodLists(){
-            axios.get('/api/goods').then(result=>{
-
+          getGoodLists(flag){
+            let param={
+              page:this.page,
+              pageSize:this.pageSize,
+              sort:this.sortFlag?1:-1
+            }
+            axios.get('/goods',{params:param}).then(result=>{
               var res = result.data
-              if(res.errno===0){
-                console.log(res)
-                this.goodList = res.data.result
+              if(res.status==="0"){
+                //flag判断是否是加载更多
+                if(flag){
+                  this.goodList = this.goodList.concat(res.result.list)
+                  if(res.result.count ===0){
+                    this.busy = true
+                  }else {
+                    this.busy = false
+                  }
+                }else {
+                  this.goodList = res.result.list
+                  this.busy = false
+                }
+
               }
               console.log(`goodList ${this.goodList}`);
             }).catch(function (error) {
@@ -113,6 +136,18 @@
           closePop(){
             this.filterBy=false
             this.overLayFlag=false
+          },
+          sortGoods(){
+            this.sortFlag = !this.sortFlag
+            this.page = 1
+            this.getGoodLists()
+          },
+          loadMore(){
+            this.page++
+            this.busy = true
+            setTimeout(() => {
+              this.getGoodLists(true)
+            }, 1000);
           }
       }
     }
